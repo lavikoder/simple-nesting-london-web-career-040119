39ade92450d6e42bf0ac5625c495978c1af10466
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-28 10:50:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,10 +119,12 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     
+     #to change the hash, assign variable for new information then move assigning hash:key:key = new_value
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = "GUI"
   
-     
+     programmer_hash
      
      
 end
